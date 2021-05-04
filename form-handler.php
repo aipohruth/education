c16d5a334dcf42cf2925ec22d5ad6a4e8fa57937
miss-lysa <82672890+miss-lysa@users.noreply.github.com>
2021-05-04 11:03:49 -0400

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,28 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$number = $_POST['number'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'miss-lysa.github.io/start-up/';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "User Number: $number.\n".
+              "Subject: $subject.\n".
+              "User Message: $message.\n";
+
+$to = 'aipohonoshozhe@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .="Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("location: contact.html");
+
+?>
